316cded731298e69029d9a16e92869a75e192952
Alexei Egorov <lightsparda.xx@gmail.com>
2019-01-30 18:00:07 +0300

    Это сайт

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script type="javascript" src="js/bootstrap.min.js"> </script>
+    <style>
+        .block1{
+            background-color: #ffa351;
+        }
+        .block2{
+            background-color: #64ff4f;
+        }
+        .block3{
+            background-color: #737fff;
+        }
+    </style>
+    <title>Вспомнить всё</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8 block1">col-sm-8</div>
+        <div class="col-sm-4 block3">col-sm-4</div>
+    </div>
+    <div class="row">
+        <div class="col-sm block1">col-sm</div>
+        <div class="col-sm block2">col-sm</div>
+        <div class="col-sm block3">col-sm</div>
+    </div>
+</div>
+
+
+</body>
+</html>
